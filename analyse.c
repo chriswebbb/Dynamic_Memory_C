ae9af971ed595d79bbcad9ad2fb2ebcf7bc4bc3c
Christian Webb <36595425+chriswebbb@users.noreply.github.com>
2021-10-27 13:15:39 +0100

    Update analyse.c

--- analyse.c
+++ analyse.c
@@ -1,11 +1,3 @@
-/*
- *         File: analyse.c
- *       Author: Steve Gunn
- *      License: MIT License
- *         Date: 27th October 2018
- *  Description: Program to read in a netlist file of a circuit, perform modified nodal analysis and display the result.
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include "circuit.h"
