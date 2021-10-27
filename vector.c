0f5876623d638443bc08dddadb0911c29bb963b7
Christian Webb <36595425+chriswebbb@users.noreply.github.com>
2021-10-27 13:17:09 +0100

    Update vector.c

--- vector.c
+++ vector.c
@@ -1,11 +1,3 @@
-/*
- *         File: vector.c
- *       Author: Steve Gunn
- *      License: MIT License
- *         Date: 27th October 2018
- *  Description: Implementation to dynamically allocate and perform operations on vectors.
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include "vector.h"
